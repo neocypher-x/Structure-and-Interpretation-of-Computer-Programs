fbfbed08c96e157cd50ce3563b4f7791ff7f0b1d
hachyon <hachyon@gmail.com>
2013-10-03 20:28:58 -0700

    finish tree recursion

--- scheme/test.scm
+++ scheme/test.scm
@@ -13,16 +13,29 @@
 		 (+ counter 1)
 		 max-count)))
 
+; Ackerman's function
+
 (define (A x y)
   (cond ((= y 0) 0)
 	((= x 0) (* 2 y))
 	((= y 1) 2)
 	(else (A (- x 1)
 		 (A x (- y 1))))))
+; f(n) = f(n-1) + 2 f(n-2) + 3 f(n-3)
+; both recursive and iterative implementations
+
 (define (f n)
   (cond ((< n 3) n)
 	((>= 3) ( + (f (- n 1)) (* 2 (f (- n 2))) (* 3 (f (- n 3))))
 	)))
+
+(define (f n)
+  (func-iter 2 1 0 n))
+
+(define (func-iter a b c count)
+  (if (= count 0)
+      c
+      (func-iter (+ a (* 2 b) (* 3 c)) a b (- count 1))))
 
 ; computes values in Pascal's triangle
 ; i is the level starting with 1 at topmost
