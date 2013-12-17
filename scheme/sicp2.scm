6fd8ca16926a5b11a997a6dd9425b995bd9d8e9c
Hachyon <hachyon@gmail.com>
2013-12-16 18:45:02 -0800

    new file

---
+++ scheme/sicp2.scm
@@ -0,0 +1,11 @@
+; 2.1
+(define (make-rat n d)
+  (let ((g (gcd n d)))
+    (cond ((and (< n 0) (< d 0)) (cons (/ (- n) g)
+				       (/ (- d) g)))
+	  ((and (> n 0) (> d 0)) (cons (/ n g)
+				       (/ d g)))
+	  ((and (< n 0) (> d 0)) (cons (/ n g)
+				       (/ d g)))
+	  ((and (> n 0) (< d 0)) (cons (/ (- n) g)
+				       (/ (- d) g))))))
