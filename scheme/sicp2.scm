44589028dee03f1ed249dc5740b2fb6755acf8bf
Hachyon <hachyon@gmail.com>
2013-12-16 19:30:05 -0800

    beginning of chapter 2

--- scheme/sicp2.scm
+++ scheme/sicp2.scm
@@ -9,3 +9,44 @@
 				       (/ d g)))
 	  ((and (> n 0) (< d 0)) (cons (/ (- n) g)
 				       (/ (- d) g))))))
+
+; 2.2
+(define (print-point p)
+  (newline)
+  (display "(")
+  (display (x-point p))
+  (display ",")
+  (display (y-point p))
+  (display ")"))
+
+(define (make-segment a b)
+  (cons a b))
+(define (start-segment x)
+  (car x))
+(define (end-segment x)
+  (cdr x))
+
+(define (make-point x y)
+  (cons x y))
+(define (x-point z)
+  (car z))
+(define (y-point z)
+  (cdr z))
+
+(define (midpoint-segment l)
+  (let ((p1 (start-segment l))
+	(p2 (end-segment l)))
+    (let ((x1 (x-point p1))
+	  (x2 (x-point p2))
+	  (y1 (y-point p1))
+	  (y2 (y-point p2)))
+    (make-point (/ (+ x1 x2) 2)
+		(/ (+ y1 y2) 2)))))
+
+(define a (make-point 1 1))
+(define b (make-point 4 4))
+(define l (make-segment a b))
+
+; 2.3
+(define (make-rect a b c)
+  
