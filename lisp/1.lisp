6e5d1af4df5036650f7344cac1fda64d7a08030b
hachyon <hachyon@gmail.com>
2013-12-16 19:30:18 -0600

    new directory structure

--- lisp/1.lisp
+++
@@ -1,9 +0,0 @@
-(defstruct polemic
-	(type (progn
-		(format t "What kind of polemic was it? ")
-		(read)))
-	(effect nil))
-
-(setf arr (make-array '(2 3) :initial-element 42))
-
-(setf vec (make-array 8 :initial-element 9000))
