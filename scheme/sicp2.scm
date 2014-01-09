a9ce6a36aea7ea5d70734d3852aaa5400c37e3db
Hachyon <hachyon@gmail.com>
2014-01-08 23:02:28 -0600

    sicp

--- scheme/sicp2.scm
+++ scheme/sicp2.scm
@@ -313,3 +313,17 @@
   (if (null? lst)
       ()
       (append (reverse_ (cdr lst)) (list (car lst)))))
+
+; 2.19
+(define us-coins (list 50 25 10 5 1))
+(define uk-coins (list 100 50 20 10 5 2 1 0.5))
+
+(define (cc amount coin-values)
+  (cond ((= amount 0) 1)
+	((or (< amount 0) (no-more? coin-values)) 0)
+	(else
+	 (+ (cc amount
+		(except-first-denomination coin-values))
+	    (cc (- amount
+		   (first-denomination coin-values))
+		coin-values)))))
