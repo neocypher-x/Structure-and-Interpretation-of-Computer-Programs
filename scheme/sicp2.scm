e62c358996f6bafc2c3c9562a3e52d34286b380d
Hachyon <hachyon@gmail.com>
2014-02-08 21:06:16 -0600

    accumulate-n

--- scheme/sicp2.scm
+++ scheme/sicp2.scm
@@ -601,17 +601,21 @@
 
 ; 2.35
 (define (enumerate-tree tree)
-  (cond ((null? tree) null)
+  (cond ((null? tree) ())
 	((not (pair? tree)) (list tree))
 	(else (append (enumerate-tree (car tree))
 		      (enumerate-tree (cdr tree))))))
-
 (define (count-leaves t)
-  (accumulate + 0 (map (lambda (x)
-			 (if (pair? x)
-			     1
-			     0))
-		       t)))
+  (accumulate + 0 (map (lambda (x) 1)
+		       (enumerate-tree t))))
 (count-leaves (list 1
 		    (list 2 (list 3 4) 5)
 		    (list 6 7)))
+
+; 2.36
+(define (accumulate-n op init seqs)
+  (if (null? (car seqs))
+      ()
+      (cons (accumulate op init (map (lambda (x) (car x)) seqs))
+	    (accumulate-n op init (map (lambda (x) (cdr x)) seqs)))))
+(accumulate-n + 0 '((1 2 3) (4 5 6) (7 8 9) (10 11 12)))
