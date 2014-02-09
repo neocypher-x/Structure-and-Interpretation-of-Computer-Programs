0c232905c9fddcb72196e3eb15c9bd1778619aff
Hachyon <hachyon@gmail.com>
2014-02-08 20:12:14 -0600

    horner evaluation

--- scheme/sicp2.scm
+++ scheme/sicp2.scm
@@ -569,7 +569,7 @@
   (if (null? s)
       (list ())
       (let ((rest (subsets (cdr s))))
-	(append rest (map list rest)))))
+	(append rest (map (lambda (x) (cons (car s) x)) rest)))))
 
 (subsets '(1 2 3))
 
@@ -585,4 +585,27 @@
   (accumulate (lambda (x y) (cons (p x) y)) () sequence))
 (map square (list 1 2 3 4 5))
 (define (append seq1 seq2)
-  (accumulate cons  __))
+  (accumulate cons seq2 seq1))
+(append '(1 2 3) '(4 5 6))
+(define (length sequence)
+  (accumulate (lambda (x y) (+ 1 y)) 0 sequence))
+(length '(1 2 3 4 5))
+
+; 2.34
+(define (horner-eval x coefficient-sequence)
+  (accumulate (lambda (this-coeff higher-terms)
+		(+ this-coeff (* x higher-terms)))
+	      0
+	      coefficient-sequence))
+(horner-eval 2 (list 1 3 0 5 0 1))
+
+; 2.35
+(define (count-leaves t)
+  (accumulate + 0 (map (lambda (x)
+			 (if (pair? x)
+			     1
+			     0))
+		       t)))
+(count-leaves (list 1
+		    (list 2 (list 3 4) 5)
+		    (list 6 7)))
