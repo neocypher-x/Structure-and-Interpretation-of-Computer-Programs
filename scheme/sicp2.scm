566a57c0deec1f3d8cf12c9c84382299e7957904
Hachyon <hachyon@gmail.com>
2014-02-14 21:13:34 -0600

    started queens

--- scheme/sicp2.scm
+++ scheme/sicp2.scm
@@ -714,3 +714,25 @@
 		    (append (list x) pair))
 		  (unique-pairs (- x 1))))
 	   (enumerate-interval 1 n)))
+
+; 2.42
+(define (queens board-size)
+  (define (queen-cols k)
+    (if (= k 0)
+	(list empty-board)
+	(filter
+	 (lambda (positions) (safe? k positions))
+	 (flatmap
+	  (lambda (rest-of-queens)
+	    (map (lambda (new-row)
+		   (adjoin-position new-row k rest-of-queens))
+		 (enumerate-interval 1 board-size)))
+	  (queen-cols (- k 1))))))
+  (queen-cols board-size))
+
+(define (adjoin-position new-row k rest-of-queens)
+  (append rest-of-queens (list new-row)))
+(define empty-board ())
+(define (safe? k positions)
+  #t)
+(queens 4)
