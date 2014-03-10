29b9e344c9b13943929909b16b696a7e98a9a95a
Hachyon <hachyon@gmail.com>
2014-03-10 15:12:50 -0500

    sets as binary trees

--- scheme/sicp2.scm
+++ scheme/sicp2.scm
@@ -1324,3 +1324,29 @@
 		       (cons x2
 			     (union-set set1
 					(cdr set2)))))))))
+; Sets as binary trees
+(define (entry tree) (car tree))
+(define (left-branch tree) (cadr tree))
+(define (right-branch tree) (caddr tree))
+(define (make-tree entry left right)
+  (list entry left right))
+
+(defien (element-of-et? x set)
+  (cond ((null? set) ())
+	((= x (entry set)) true)
+	((< x (entry set))
+	 (element-of-set? x (left-branch set)))
+	((> x (entry set))
+	 (element-of-set? x (right-branch set)))))
+
+(define (adjoin-set x set)
+  (cond ((null? set) (make-tree x '() '()))
+	((= x (entry set)) set)
+	((< x (entry set))
+	 (make-tree (entry set)
+		    (adjoin-set x (left-branch set))
+		    (right-branch set)))
+	((> x (entry set))
+	 (make-tree (entry set)
+		    (left-branch set)
+		    (adjoin-set x (right-branch set))))))
