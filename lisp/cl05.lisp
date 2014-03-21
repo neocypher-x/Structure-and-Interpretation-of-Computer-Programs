a47aef6d6151515cfc0e66e3c1e80b4fb1ad46b7
Hachyon <hachyon@gmail.com>
2014-03-21 13:22:14 -0500

    first four problems done

--- lisp/cl05.lisp
+++ lisp/cl05.lisp
@@ -3,6 +3,14 @@
 (let (x y)
   (list x y))
 
+(defconstant month
+  #(0 31 59 90 120 151 181 212 273 304 365))
+
+(defun leap? (y)
+  (and (zerop (mod y 4))
+       (or (zerop (mod y 400))
+	   (not (zerop (nmod y 100))))))
+
 ; 1
 (setf y '(1 2 3))
 (let ((x (car y)))
@@ -31,8 +39,33 @@
       (format t "inner"))
     2))
  1)
+n
+; 2
+(defun mystery (x y)
+  (if (null y)
+      nil
+      (if (eql (car y) x)
+	  0
+	  (let ((z (mystery x (cdr y))))
+	    (and z (+ z 1))))))
+(defun mystery (x y)
+  (cond ((null y) nil)
+	((eql (car y) x) 0)
+	(t (let ((z (mystery x (cdr y))))
+	     (and z (+ z 1))))))
 
 ; 3
 (defun sq-pos (x)
   (if (> x 5)
       (* x x)))
+
+; 4
+(defun month-sum (m y)
+  (+ (svref month (- m 1))
+     (if (and (> m 2) (leap? y)) 1 0)))
+(defun month-sum (m y)
+  (let ((days (case m (1 0) (2 31) (3 59)
+		(4 90) (5 120) (6 151) (7 181)
+		(8 212)	(9 243) (10 273)
+		(11 304) (12 334))))
+    (+ days (if (and (> m 2) (leap? y)) 1 0))))
