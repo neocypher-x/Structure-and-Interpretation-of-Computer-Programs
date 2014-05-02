531fd384571079e159b70d5073f3cfe7b634076f
Hachyon <hachyon@gmail.com>
2014-05-01 19:53:55 -0500

    considered coercion of the same type in apply-generic 2.81

--- scheme/sicp2.scm
+++ scheme/sicp2.scm
@@ -1877,3 +1877,69 @@
 (put '=zero? '(complex) =zero?-complex)
 (define (=zero? x)
   (apply-generic =zero? x))
+
+; 2.81
+; a
+; An infinite recursive call stack would result;
+; apply generic would get called infinitely many times.
+
+; b
+; apply generic works fine as it stands. In the case
+; where we pass in two arguments of the same type to
+; apply generic, consider two possibilities. If the
+; generic procedure exists, then we are done. If no
+; such operation exists, get-coercion would not return
+; a coercion procedure. This would result in apply
+; generic returning error saying that no method exists
+; for the two types. In both cases, the return is as
+; expected.
+
+; c
+(define (apply-generic op . args)
+  (let ((type-tags (map type-tag args)))
+    (let ((proc (get op type-tags)))
+      (if proc
+          (apply proc (map contents args))
+          (if (= (length args) 2)
+              (let ((type1 (car type-tags))
+                    (type2 (cadr type-tags))
+                    (a1 (car args))
+                    (a2 (cadr args)))
+		(if (not (eq? type1 type2))
+		    (let ((t1->t2 (get-coercion type1 type2))
+			  (t2->t1 (get-coercion type2 type1)))
+		      (cond (t1->t2
+			     (apply-generic op (t1->t2 a1) a2))
+			    (t2->t1
+			     (apply-generic op a1 (t2->t1 a2)))
+			    (else
+			     (error "No method for these types"
+				    (list op type-tags)))))
+		    (error "No method for same types"
+			   (list op type-tags))))
+	      (error "No method for these types"
+		     (list op type-tags)))))))
+
+; 2.82
+(define (apply-generic op . args)
+  (let ((type-tags (map type-tag args)))
+    (let ((proc (get op type-tags)))
+      (if proc
+          (apply proc (map contents args))
+          (if (= (length args) 2)
+              (let ((type1 (car type-tags))
+                    (type2 (cadr type-tags))
+                    (a1 (car args))
+                    (a2 (cadr args)))
+                (let ((t1->t2 (get-coercion type1 type2))
+                      (t2->t1 (get-coercion type2 type1)))
+                  (cond (t1->t2
+                         (apply-generic op (t1->t2 a1) a2))
+                        (t2->t1
+                         (apply-generic op a1 (t2->t1 a2)))
+                        (else
+                         (error "No method for these types"
+                                (list op type-tags))))))
+              (error "No method for these types"
+                     (list op type-tags)))))))
+; 
