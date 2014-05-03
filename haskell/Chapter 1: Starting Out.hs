7dfa51246693c713d3a7a9f51d86fdbfe110747c
Hachyon <hachyon@gmail.com>
2014-05-03 18:43:37 -0500

    revisit list comprehensions

--- haskell/Chapter 1: Starting Out.hs
+++ haskell/Chapter 1: Starting Out.hs
@@ -13,3 +13,11 @@ conanO'Brien = "It's a me-, Conan O'Brien!"
 lostNumbers = [4,8,15,16,23,42]
 
 boomBangs xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
+
+not13or15or19 = [ x | x <- [10..20], x/= 13, x /= 15, x/= 19]
+
+allCombinationsSum = [ x+y | x <- [1,2,3], y <- [10,100,1000]]
+
+nouns = ["hobo","frog","pope"]
+adjectives = ["lazy", "grouchy", "scheming"]
+combineNounsAdjectives = [adjective ++ " " ++ noun | adjective <- adjectives, noun <- nouns]
