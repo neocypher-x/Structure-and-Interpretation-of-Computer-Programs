0798988c33305734630d241dc0f6fccb5755679c
Hachyon <hachyon@gmail.com>
2014-12-11 19:59:06 -0600

    queue as a procedure with local state

--- Chapter3.scm
+++ Chapter3.scm
@@ -842,20 +842,23 @@
 ;         |______|                |________|
 ;        call to car           call to dispatch
 
+; helpers
 (define (front-ptr queue) (car queue))
 (define (rear-ptr queue) (cdr queue))
 (define (set-front-ptr! queue item) (set-car! queue item))
 (define (set-rear-ptr! queue item) (set-cdr! queue item))
 
-(define (empty-queue? queue) (null? (front-ptr queue)))
-
+; constructor
 (define (make-queue) (cons '() '()))
 
+; selectors
+(define (empty-queue? queue) (null? (front-ptr queue)))
 (define (front-queue queue)
   (if (empty-queue? queue)
       (error "FRONT called with an empty queue" queue)
       (car (front-ptr queue))))
 
+; mutators
 (define (insert-queue! queue item)
   (let ((new-pair (cons item '())))
     (cond ((empty-queue? queue)
@@ -866,7 +869,6 @@
 	   (set-cdr! (rear-ptr queue) new-pair)
 	   (set-rear-ptr! queue new-pair)
 	   queue))))
-
 (define (delete-queue! queue)
   (cond ((empty-queue? queue)
 	 (error "DELETE! called with an empty queue" queue))
@@ -905,3 +907,57 @@
 
 (define (print-queue queue)
   (display (front-ptr queue)))
+
+; 3.22
+(define (make-queue)
+  (let ((front-ptr '())
+	(rear-ptr '()))
+    (define (empty-queue?)
+      (null? front-ptr))
+    (define (front-queue)
+      (if (empty-queue?)
+	  (error "FRONT called with an empty-queue")
+	  (car front-ptr)))
+    (define (insert-queue! item)
+      (let ((new-pair (cons item '())))
+	(cond ((empty-queue?)
+	       (set! front-ptr new-pair)
+	       (set! rear-ptr new-pair)
+	       dispatch)
+	      (else
+	       (set-cdr! rear-ptr new-pair)
+	       (set! rear-ptr new-pair)
+	       dispatch))))
+    (define (delete-queue!)
+      (cond ((empty-queue?) (error "DELETE! called with an empty queue" dispatch))
+	    (else
+	     (set! front-ptr (cdr front-ptr))
+	     dispatch)))
+    (define (dispatch m)
+      (cond ((eq? m 'empty-queue?) (empty-queue?))
+	    ((eq? m 'front-queue) (front-queue))
+	    ((eq? m 'insert-queue!) insert-queue!)
+	    ((eq? m 'delete-queue!) (delete-queue!))
+	    (else (error "Undefined operation -- MAKE-QUEUE" m))))
+    dispatch))
+
+; public methods
+(define (empty-queue? queue)
+  (queue 'empty-queue?))
+(define (front-queue queue)
+  (queue 'front-queue))
+(define (insert-queue! queue item)
+  ((queue 'insert-queue!) item))
+(define (delete-queue! queue)
+  (queue 'delete-queue!))
+
+; tests
+(define q1 (make-queue))
+(empty-queue? q1)
+; #t
+(insert-queue! q1 1)
+(front-queue q1)
+; 1
+(delete-queue! q1)
+(empty-queue? q1)
+; #t
